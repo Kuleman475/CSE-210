82a7a9c6f7c89c9be7f08af7a7baadfdea477c08
Use your GitHub username <Enter your email address>
2024-10-02 14:02:56 -0400

    week 3 _text

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,45 +4,56 @@ using System;
         private Reference _reference;
         
         private List<Word> _words;
-        
+        private List<string> _text;
 
+        private List<string> _text2;
     public Scripture(string book, int chapter, int verse, string text)
         {
             _reference = new Reference(book, chapter, verse);
             _words = new List<Word>();
+            _text = new List<string>();
             string[] splitText = text.Split(' ');
             foreach (string word in splitText)
             {
                 _words.Add(new Word(word));
+                _text.Add(word + " ");
             }
             
         }
     
-        public void HideRandomWords(int numberToHide, string text) {
-            // Word word = new Word(text);
-            // Console.WriteLine(text);
-            // Console.Write(_words.Count);
-            List<string> visibleWords = new List<string>();
+        public void HideRandomWords(int numberToHide) {
+
             
-            foreach (var word1 in text){
-                Console.WriteLine(word1);
-            }
             Random random = new Random();
-            int index1 = random.Next(_words.Count);
-            int index2 = random.Next(_words.Count);
-            int index3 = random.Next(_words.Count);
-            int index4 = random.Next(_words.Count);
-            int index5 = random.Next(_words.Count);
-            Console.WriteLine($"{index1}, {index2}, {index3}, {index4}, {index5}");
-            // foreach(Word word1 in _words)
-            // {
-            //     Console.WriteLine(word1);
-            // }
-             
+            int index1 = random.Next(_text.Count);
+            int index2 = random.Next(_text.Count);
+            int index3 = random.Next(_text.Count);
+            int index4 = random.Next(_text.Count);
+            int index5 = random.Next(_text.Count);
+            // Console.Write($"{_text[index1]} {_text[index2]}"
+            // + $"{_text[index3]} {_text[index4]} {_text[index5]}");
+            _text2 = new List<string>();
+            foreach(string word in _text)
+            {
+                
+                if(_text[index1] == word) {
+                    foreach(char letter in word)
+                        _text2.Add("_");
+                }
+                else {
+                    _text2.Add(word);
+                }
+            }
+            // Console.WriteLine(_text2);
+            for (int i = 0; i < _text.Count; i++) 
+                {
+                Console.Write(_text2[i]);
+                }
         }
+
         public string GetDisplayText(){
-              
-             return _words.Count.ToString();
+
+            return _text.ToString();
             
         }
     // public bool IsCompletelyHidden(){

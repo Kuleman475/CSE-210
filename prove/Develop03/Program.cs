0203069cd75f25dfa5e565fd9ebf6cc9e376b1b9
Taylor Christensen <kuleman47@byui.edu>
2024-10-02 23:00:12 -0400

    finished

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,30 +1,50 @@
 using System;
 using System.Reflection.Metadata;
 using System.Transactions;
-
+class Program
+{
+    static void Main(string[] args)
+    {
 string text = "And now it came to pass that all this was done in Mormon, yea, by the waters of Mormon, in the forest \r\n"
      + "that was near the waters of Mormon; yea, the place of Mormon, the waters of Mormon, the forest of Mormon, how beautiful \r\n"
      + "are they to the eyes of them who there came to the knowledge of their Redeemer; yea, and how blessed are they, for \r\n"
      + "they shall sing to his praise forever. ";
+
     Word word = new Word(text);
     Reference reference = new Reference("Mosiah ", 18, 30);
     Scripture scripture = new Scripture("Mosiah ", 18, 30, text);
     Console.Clear();
     Console.Write(reference.GetDisplayText());
     Console.Write(word.GetDisplayText());
-
-    // Console.ReadKey();
-    Console.WriteLine();
-    Console.Write("Press enter to continue or type 'quit' to quit:  ");
-    string userInput = Console.ReadLine();
-
-    while (userInput != "quit") {
-        if (Console.ReadKey().Key == ConsoleKey.Enter){
+    int a = 0;
+    while (a < 71){
+        Console.WriteLine($"\n");
+        Console.Write("Press enter to continue or type 'quit' to quit:  ");
+        string userInput = Console.ReadLine();
+        Console.Clear();
+        Console.Write(reference.GetDisplayText());
+        
+        scripture.HideRandomWords(5);
+        a+=5;
+        if (a >= 70) {
             Console.Clear();
-            scripture.HideRandomWords(5);
-            }
+            a += 70;
+            
+        Console.WriteLine("Congratulations you did it!"); 
+        }
+        if (userInput == "quit") {
+            Console.Clear();
+            a += 77;
+            
+        Console.WriteLine("Goodbye"); 
+        }
 
+        }
     }
-    if (userInput == "quit"){
-        Console.WriteLine("GOODBYE!");
+        
     }
+    
+                
+            
+    
+    
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -10,94 +10,65 @@ using System;
         public List<int> _numbers;
 
     public Scripture(string book, int chapter, int verse, string text)
+    {
+        _reference = new Reference(book, chapter, verse);
+        _words = new List<Word>();
+        _text = new List<string>();
+        // _numbers = new List<int>();
+        string[] splitText = text.Split(' ');
+        foreach (string word in splitText)
         {
-            _reference = new Reference(book, chapter, verse);
-            _words = new List<Word>();
-            _text = new List<string>();
-            // _numbers = new List<int>();
-            string[] splitText = text.Split(' ');
-            foreach (string word in splitText)
-            {
-                _words.Add(new Word(word));
-                _text.Add(word);
-            }
-            _numbers = new List<int>();
+            _words.Add(new Word(word));
+            _text.Add(word);
         }
-    
-        public void HideRandomWords(int numberToHide) {
+        _numbers = new List<int>();
+    }
 
-            
-            Random random = new Random();
-            int index1 = random.Next(_text.Count-1);
-            int index2 = random.Next(_text.Count-1);
-            int index3 = random.Next(_text.Count-1);
-            int index4 = random.Next(_text.Count-1);
-            int index5 = random.Next(_text.Count-1);
-            // Console.Write($"{_text[index1]} {_text[index2]}"
-            // + $"{_text[index3]} {_text[index4]} {_text[index5]}");
-            _text2 = new List<string>();
+    public void HideRandomWords(int numberToHide) {
 
-            for (int i = 0; i <= numberToHide-1;) {
-                if (_numbers.Contains(index1)){
-                index1 = random.Next(_text.Count-1);
-            }
-                else {
-                    _numbers.Add(index1);
-                    i++;
-                }
-            } 
-            Console.WriteLine(_numbers.Count);
-            
-            foreach(string word in _text)
-            {
-                
-        _text2.Add(word + " ");
-                    // int letter = _text[num].Length; 
-                    //  string replaceWord = new string ('_', letter);
-                    // _text2.Add(replaceWord + " ");
-                    // }
-                    // else{
-                    //      
-                    // }
-            }
-            for (int i = 0; i < _text.Count; i++) {
-            if(_numbers.Contains(i)) {
-                int letter = _text[i].Length; 
-                     string replaceWord = new string ('_', letter);
-            Console.Write(_text2[i].Replace(_text[i], replaceWord));
-            }
+        
+        Random random = new Random();
+        int index1 = random.Next(_text.Count-1);
+        _text2 = new List<string>();
+
+        for (int i = 0; i <= numberToHide-1;) {
+            if (_numbers.Contains(index1)){
+            index1 = random.Next(_text.Count-1);
+        }
             else {
-            Console.Write(_text2[i]);
-
+                _numbers.Add(index1);
+                i++;
             }
-        }
-                // if(_numbers.Contains(index1)) {
-                //     int letter = _text[index1].Length;       
-                //     string replaceWord = new string ('_', letter);
-                //     _text2.Add(replaceWord + " ");
-                // }
-                // else {
-                   
-                // }
+        } 
+        
+        foreach(string word in _text)
+        {
             
-           foreach(int num in _numbers){
-            //     Console.Write(num +", ");
-            
-            }
-            // for (int i = 0; i < _text.Count; i++) 
-            //     {
-            //     Console.Write(_text2[i].Replace(_text[10], "10"));
-            //     }
+    _text2.Add(word + " ");
         }
-
-
-
-        public string GetDisplayText(){
-
-            return _text.ToString();
-            
+        for (int i = 0; i < _text.Count; i++) {
+        if(_numbers.Contains(i)) {
+            int letter = _text[i].Length; 
+                    string replaceWord = new string ('_', letter);
+        Console.Write(_text2[i].Replace(_text[i], replaceWord));
         }
-    // public bool IsCompletelyHidden(){
+        else {
+        Console.Write(_text2[i]);
 
-    // }
+        }
+    }
+        if (_numbers.Count >= 74){
+            IsCompletelyHidden();
+            Console.WriteLine("Congratulations you did it!!");
+        }
+    }
+
+    public string GetDisplayText(){
+
+        return _text.ToString();
+        
+    }
+public bool IsCompletelyHidden(){
+    return true;
+}
 }

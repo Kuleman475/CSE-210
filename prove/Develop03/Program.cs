bac5a01ff5d4c0e76d09e801686b099ac22dd448
Use your GitHub username <Enter your email address>
2024-10-03 07:55:38 -0400

    Week 3 Complete

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,6 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
+
+// for the  Exceeding Requirements I had the program close once you have no words left.
+
 string text = "And now it came to pass that all this was done in Mormon, yea, by the waters of Mormon, in the forest \r\n"
      + "that was near the waters of Mormon; yea, the place of Mormon, the waters of Mormon, the forest of Mormon, how beautiful \r\n"
      + "are they to the eyes of them who there came to the knowledge of their Redeemer; yea, and how blessed are they, for \r\n"

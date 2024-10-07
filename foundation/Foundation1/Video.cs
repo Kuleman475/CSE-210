9de6a8dde8ed775531d8c778496b87b9138ad1cf
Use your GitHub username <Enter your email address>
2024-10-07 14:21:11 -0400

    Video Class

---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,6 @@
+class Video{
+ //   Your program should have a class for a Video that has the responsibility to track 
+ //   the title, author, and length (in seconds) of the video. 
+ //   Each video also has responsibility to store a list of comments, and 
+ //   should have a method to return the number of comments.
+}
